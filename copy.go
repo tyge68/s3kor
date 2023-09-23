b673549bfe254d3aecc69d2df26db466fe1620af
Thierry Ygé <tyge@adobe.com>
2023-09-23 10:40:08 +0200

    fix: error log

--- copy.go
+++ copy.go
@@ -151,6 +151,7 @@ func (cp *BucketCopier) copyFile(file fileJob) {
 			}
 		}
 	} else {
+		fmt.Println("AWS Key:" + cp.dest.Path + "/" + file.path)
 		// Upload the file to S3.
 		input := cp.template
 		input.Key = aws.String(cp.dest.Path + "/" + file.path)
